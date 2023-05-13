61c696c51bca14bbf023845334ac97ce5015497d
kanowory <kanowory@student.wszib.edu.pl>
2023-05-13 12:30:44 +0200

    Strona główna

---
+++ src/main/java/pl/wszib/pizzamarket/web/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package pl.wszib.pizzamarket.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage(Model model){
+        model.addAttribute("firstName", "Adam");
+        return "homePage";
+    }
+
+}
